35fccf42cf88e16d26d6880885047966172e5dd9
Jeroen Oomkes <oomkes@xparvision.com>
2020-05-29 16:41:08 +0200

    Fixed bug in case application closes. It is not possible to use a blocking call on deleteLater for worker in case app closes. "If deleteLater() is called after the main event loop has stopped, the object will not be deleted". And that's why it waits forever.

--- src/websocketconnection.cpp
+++ src/websocketconnection.cpp
@@ -22,9 +22,9 @@ WebSocketConnection::WebSocketConnection(QObject* parent) : QThread(parent), _wo
 }
 WebSocketConnection::~WebSocketConnection()
 {
-    QMetaObject::invokeMethod(_worker, "deleteLater", Qt::BlockingQueuedConnection);
     exit();
     wait();
+    delete _worker;
 }
 void WebSocketConnection::close()
 {
